786af4170bd1bc6ef9e900d5f4140962cf770717
Mathieu Virbel <mathieu@pymt.eu>
2010-10-08 10:01:35 +0200

    ajout de la possibilité de démarrer directement sur une série de fichier au lieu de tout scanner

--- start.py
+++ start.py
@@ -1,11 +1,18 @@
+import sys
 from pymt import *
 from logotouch import LTWorld
 from os.path import join, dirname
 from glob import glob
 
 world = LTWorld()
-datapath = join(dirname(__file__), 'data')
-for filename in glob(join(datapath, '*.csv')):
+
+if len(sys.argv) > 1:
+    files = sys.argv[1:]
+else:
+    datapath = join(dirname(__file__), 'data')
+    files = glob(join(datapath, '*.csv'))
+
+for filename in files:
     world.create_word(filename=filename)
 
 runTouchApp(world)
