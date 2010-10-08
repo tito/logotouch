976a52040dedbb834d8ea6cb5d3e27058e33d1a8
Mathieu Virbel <mathieu@pymt.eu>
2010-10-08 10:01:57 +0200

    n'affiche pas le pronom ou la possibilité de temps en fonction du type

--- logotouch/wordprovider.py
+++ logotouch/wordprovider.py
@@ -1,14 +1,21 @@
 import csv
+import os
 
 class AbstractWordProvider(object):
 
     def __init__(self):
         #TODO:  nedd type..like verb, adjective, noun etc?!
         self.data = {}
+        self.is_verb = False
+        self.wtype = 'verbe' # verbe, mot, adverbe
         self.word = 'walk'
         self.tense = 1 # 0=past, 1=present, 2=future
+        self.show_person = True
         self.person = 1
         self.pronouns = [u'Je', u'Tu', u'Il', u'Nous', u'Vous', u'Ils']
+        self.pronouns_word = [u'un', u'le', u'ce', u'une', u'la', u'cette',
+                              u'des', u'les', u'ces', u'ma', u'sa', u'ta',
+                              u'nos', u'notre', u'leurs', u'leur']
         self.zoom = 0
         self.maxzoom = 0
         self.minzoom = 0
@@ -18,6 +25,14 @@ class AbstractWordProvider(object):
 
     def __str__(self):
         return self.pronouns[self.person] + self.conjugate() + ' ' + str(self.tense)
+
+    @property
+    def use_pronouns(self):
+        return self.wtype in ('verbe', 'mot')
+
+    @property
+    def use_time(self):
+        return self.wtype in ('verbe', )
 
     def conjugate(self):
         #TODO: return right word form root with time and person
@@ -55,6 +70,8 @@ class AbstractWordProvider(object):
         return True
 
     def do_time_next(self):
+        if not self.use_time:
+            return
         tense = min(2, self.tense + 1)
         if tense == self.tense:
             return
@@ -62,20 +79,30 @@ class AbstractWordProvider(object):
         return True
 
     def do_time_previous(self):
+        if not self.use_time:
+            return
         tense = max(0, self.tense - 1)
         if tense == self.tense:
             return
         self.tense = tense
         return True
 
+    def toggle_person(self):
+        self.show_person = not self.show_person
+
     def do_person_next(self):
-        person = min(len(self.pronouns) - 1, self.person + 1)
+        if not self.use_pronouns:
+            return
+        pronouns = self.pronouns if self.wtype == 'verbe' else self.pronouns_word
+        person = min(len(pronouns) - 1, self.person + 1)
         if person == self.person:
             return
         self.person = person
         return True
 
     def do_person_previous(self):
+        if not self.use_pronouns:
+            return
         person = max(0, self.person - 1)
         if person == self.person:
             return
@@ -88,17 +115,22 @@ class CSVWordProvider(AbstractWordProvider):
         super(CSVWordProvider, self).__init__()
         self.filename = filename
         self.load()
+        ext = os.path.basename(filename).split('-')[0]
+        self.wtype = ext
 
     def load(self):
         def norm(title):
+            title = title.lower()
             c = title.split()
             if len(c) != 2:
+                title = title.replace('nom', 'name')
+                title = title.replace('adverbe', 'name')
                 return title
             a, b = c
-            a = a.replace('Pr\xc3\xa9sent', 'present')
-            a = a.replace('Futur', 'future')
-            a = a.replace('Imparfait', 'past')
-            a = a.replace('Imparfai', 'past')
+            a = a.replace('pr\xc3\xa9sent', 'present')
+            a = a.replace('futur', 'future')
+            a = a.replace('imparfait', 'past')
+            a = a.replace('imparfai', 'past')
             d = {'je': 0, 'tu': 1, 'il': 2, 'nous': 3, 'vous': 4, 'ils': 5}
             return '%s_%s' % (a, d[b.lower()])
         rows = []
@@ -116,8 +148,8 @@ class CSVWordProvider(AbstractWordProvider):
 
         self.titles = titles
         for row in rows:
+            print self.filename, row[0]
             self.data[norm(row[0])] = row[1:]
-
 
         # count zoomin
         for t in titles:
@@ -152,10 +184,20 @@ class CSVWordProvider(AbstractWordProvider):
                 title = 'secouer%d' % self.synonym
 
         # tense ?
-        a = {0: 'past', 1: 'present', 2: 'future'}
-        tense = '%s_%d' % (a[self.tense], self.person)
+        if self.wtype == 'verbe':
+            a = {0: 'past', 1: 'present', 2: 'future'}
+            tense = '%s_%d' % (a[self.tense], self.person)
+        else:
+            tense = 'name'
 
         tidx = self.titles.index(title) - 1
         word = self.data[tense][tidx]
         word = word.decode('utf8')
-        return self.pronouns[self.person] + u' ' + word
+
+        if self.show_person and self.use_pronouns:
+            if self.wtype == 'verbe':
+                return self.pronouns[self.person] + u' ' + word
+            else:
+                return self.pronouns_word[self.person] + u' ' + word
+        else:
+            return word
